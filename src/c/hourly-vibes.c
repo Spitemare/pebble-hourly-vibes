4c3f5ad9741f57041102e68b14c772c237d590b0
David Morgan <dmorgan81@gmail.com>
2016-10-25 14:55:49 -0400

    Unsub from tick timer service if user is sleeping

--- src/c/hourly-vibes.c
+++ src/c/hourly-vibes.c
@@ -35,9 +35,10 @@ static void prv_health_event_handler(HealthEventType event, void *context) {
     } else if (event == HealthEventSleepUpdate) {
         HealthActivityMask mask = health_service_peek_current_activities();
         bool sleeping = (mask & HealthActivitySleep) || (mask & HealthActivityRestfulSleep);
-        if (sleeping && s_tick_timer_event_handle) {
-            events_tick_timer_service_unsubscribe(s_health_event_handle);
-        } else if (!sleeping && !s_tick_timer_event_handle) {
+        if (sleeping && s_tick_timer_event_handle != NULL) {
+            events_tick_timer_service_unsubscribe(s_tick_timer_event_handle);
+            s_tick_timer_event_handle = NULL;
+        } else if (!sleeping && s_tick_timer_event_handle == NULL) {
             s_tick_timer_event_handle = events_tick_timer_service_subscribe(HOUR_UNIT, prv_tick_handler);
         }
     }
